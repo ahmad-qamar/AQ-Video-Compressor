eb2421395d0d1d322ae8a0923f32c94cc60f22ae
Ahmad Qamar <34595464+ahmad-qamar@users.noreply.github.com>
2022-08-13 00:58:25 +0500

    Working ListView implementation after adding videos from the designated button

--- Video Compressor/MainPage.xaml.cs
+++ Video Compressor/MainPage.xaml.cs
@@ -2,35 +2,39 @@
 
 public partial class MainPage : ContentPage
 {
-
     public MainPage()
     {
         InitializeComponent();
+        listView.BindingContext = this;
     }
-
-    private void OnCounterClicked(object sender, EventArgs e)
+    private async void OnCounterClicked(object sender, EventArgs e)
     {
-        async Task<List<string>> PickAndShow(PickOptions options)
+        async Task<IEnumerable<string>> PickAndShow(PickOptions options)
         {
             try
             {
                 var result = await FilePicker.Default.PickMultipleAsync(options);
 
-                return result.Select(r => r.FullPath).ToList();
+                return result.Select(r => r.FullPath).AsEnumerable();
             }
             catch (Exception ex)
             {
                 // The user canceled or something went wrong
+                return null;
             }
 
             return null;
         }
-        PickAndShow(new PickOptions { FileTypes = FilePickerFileType.Videos, PickerTitle = "Select videos." });
+        VideoLocations.AddRange(await PickAndShow(new PickOptions { FileTypes = FilePickerFileType.Videos, PickerTitle = "Select videos." }));
+        listView.ItemsSource = null;
+        listView.ItemsSource = VideoLocations;
     }
+    public List<string> VideoLocations { get; set; } = new();
+    public record VideoLocation(string Path);
 
     private void StartBtn_Clicked(object sender, EventArgs e)
     {
-
+        
     }
 
     private void StopBtn_Clicked(object sender, EventArgs e)
